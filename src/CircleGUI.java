97882381cde9b68a6447d2019c6c4fefcc4d86ee
Le Bronn <lebronn.samson@cit.edu>
2022-12-12 11:30:58 +0800

    Added solutions to the circleGUI.

--- src/CircleGUI.java
+++ src/CircleGUI.java
@@ -15,4 +15,19 @@ public class CircleGUI extends JFrame{
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setVisible(true);
     }
+
+    public CircleGUI() {
+        btnCompute.addActionListener(e -> {
+            double radius = Double.parseDouble(tfRadius.getText());
+            double area = Math.PI * Math.pow(radius, 2);
+            double circumference = 2 * Math.PI * radius;
+            tfArea.setText(String.valueOf(String.format("%.2f", area)));
+            tfCircumference.setText(String.valueOf(String.format("%.2f", circumference)));
+        });
+        btnClear.addActionListener(e -> {
+            tfRadius.setText("");
+            tfArea.setText("");
+            tfCircumference.setText("");
+        });
+    }
 }
